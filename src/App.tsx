781e8f4aeaaf4ceec8e9da59c1ce3ede07258771
sketaro <s.ryutaro1995.6.24@gmail.com>
2023-01-30 14:31:53 +0000

    update part4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
+import Loading from "./components/Loading";
 import Results from "./components/Results";
 import Title from "./components/Title";
 
@@ -13,6 +14,7 @@ type ResultsStateType = {
 };
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
   const [city, setCity] = useState<string>("");
   const [results, setResults] = useState<ResultsStateType>({
     country: "",
@@ -23,6 +25,7 @@ function App() {
   });
   const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
     fetch(
       `https://api.weatherapi.com/v1/current.json?key=d8b679fb06e1416cb44143127232901&q=${city}&aqi=no`
     )
@@ -35,14 +38,21 @@ function App() {
           conditionText: data.current.condition.text,
           icon: data.current.condition.icon,
         });
-      });
+        setCity("");
+        setLoading(false);
+      })
+      .catch((err) =>
+        alert(
+          "エラーが発生しました。ページをリロードして、もう一度トライしてください。"
+        )
+      );
   };
   return (
     <div className="wrapper">
       <div className="container">
         <Title></Title>
-        <Form setCity={setCity} getWeather={getWeather}></Form>
-        <Results results={results}></Results>
+        <Form city={city} setCity={setCity} getWeather={getWeather}></Form>
+        {loading ? <Loading></Loading> : <Results results={results}></Results>}
       </div>
     </div>
   );
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -8,27 +8,24 @@ type ResultsPropsType = {
   };
 };
 
-const Results = (props: ResultsPropsType) => {
+const Results = ({ results }: ResultsPropsType) => {
+  const { country, cityName, temperature, conditionText, icon } = results;
   return (
-    <div>
-      {props.results.country && (
-        <div className="results-country">{props.results.country}</div>
-      )}
-      {props.results.cityName && (
-        <div className="results-city">{props.results.cityName}</div>
-      )}
-      {props.results.temperature && (
+    <>
+      {country && <div className="results-country">{country}</div>}
+      {cityName && <div className="results-city">{cityName}</div>}
+      {temperature && (
         <div className="results-temp">
-          {props.results.temperature} <span>℃</span>
+          {temperature} <span>℃</span>
         </div>
       )}
-      {props.results.conditionText && (
+      {conditionText && (
         <div className="results-condition">
-          <img src={props.results.icon} alt="icon" />
-          <span>{props.results.conditionText}</span>
+          <img src={icon} alt="icon" />
+          <span>{conditionText}</span>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
